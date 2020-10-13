870459c4e4e63a3affbe22db54351cd8c1334432
sourav-browserstack <72795821+sourav-browserstack@users.noreply.github.com>
2020-10-13 17:19:56 +0530

    Update automate_test.rb

--- testScripts/automate_test.rb
+++ testScripts/automate_test.rb
@@ -12,11 +12,11 @@ caps["os_version"] = "Catalina"
 caps["browser"] = "Safari"
 caps["browser_version"] = "13.1"
 caps["resolution"] = "1920x1080"
-caps["build"] = ENV['BROWSERSTACK_BUILD_NAME']
-caps["project"] = ENV['BROWSERSTACK_PROJECT_NAME']
-caps["name"] = "myapp test 3"
-caps["browserstack.local"] = "true"
-caps["browserstack.localIdentifier"] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
+# caps["build"] = ENV['BROWSERSTACK_BUILD_NAME']
+# caps["project"] = ENV['BROWSERSTACK_PROJECT_NAME']
+caps["name"] = "Azure pipeline test"
+# caps["browserstack.local"] = "true"
+# caps["browserstack.localIdentifier"] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
 caps["browserstack.debug"] = "true"
 caps["browserstack.networkLogs"] = "true"
 caps["browserstack.timezone"] = "New York"
@@ -27,7 +27,27 @@ driver = Selenium::WebDriver.for(:remote,
 	:url => url,
 	:desired_capabilities => caps)
 
-driver.navigate.to "http://localhost:3000"
+# driver.navigate.to "http://localhost:3000"
+# puts driver.title
+# driver.close()
+# driver.quit()
+
+# Setting name of the test
+driver.execute_script('browserstack_executor: {"action": "setSessionName", "arguments": {"name": "Ruby test"}}')
+
+# Searching for 'BrowserStack' on google.com
+driver.navigate.to "http://www.google.com"
+element = driver.find_element(:name, "q")
+element.send_keys "BrowserStack"
+element.submit
+
 puts driver.title
-driver.close()
-driver.quit()
+
+# Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page matches 'BrowserStack - Google Search'
+if driver.title=="BrowserStack - Google Search"
+  driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Title matched!"}}')
+else
+  driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Title not matched"}}')
+end
+
+driver.quit
